e460678bcf9fa289becfb2e427f055ec2b50c9c7
Chhavi <chhavig072@gmail.com>
2026-05-05 09:56:08 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,37 @@
+import { useState } from "react";
+import "./App.css";
 
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="container">
+      <h1>Counter App</h1>
+      <h2>{count}</h2>
+
+      <div className="buttons">
+        <button onClick={increase}>Increment</button>
+
+        <button onClick={decrease} disabled={count === 0}>
+          Decrease
+        </button>
+
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
